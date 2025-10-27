a7b5ce7d8c8c3a0e9ccabd397f1eb70f31769b9d
Tsubaki Qiaice <boyi.zh.life@outlook.com>
2025-10-27 16:06:49 +0800

    jpa: 搭建一个基础的 jpa 项目

---
+++ jpa/src/main/java/org/qiaice/runner/MainRunner.java
@@ -0,0 +1,13 @@
+package org.qiaice.runner;
+
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MainRunner implements ApplicationRunner {
+
+    @Override
+    public void run(ApplicationArguments args) {
+    }
+}
